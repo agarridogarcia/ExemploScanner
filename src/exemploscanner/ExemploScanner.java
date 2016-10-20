fffe06587b7eb684a90faa47c3417a9a814df045
agarridogarcia <agarridogarcia@atanasoff30.danielcastelao.org>
2016-10-20 12:09:04 +0200

    scanner

---
+++ src/exemploscanner/ExemploScanner.java
@@ -0,0 +1,16 @@
+
+package exemploscanner;
+
+import java.util.Scanner;
+
+public class ExemploScanner {
+
+    
+    public static void main(String[] args) {
+        Scanner obxecto=new Scanner(System.in);
+        System.out.println("Teclea variable");
+        int variable = obxecto.nextInt();
+        System.out.println("variable" + variable);
+    }
+    
+}
